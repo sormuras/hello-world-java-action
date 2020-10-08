d6c55d50c4fa4689bb0732197a353901b7dd599c
Christian Stein <sormuras@gmail.com>
2020-10-08 12:45:18 +0200

    Clean up

--- Action.java
+++ Action.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class Action {
   public static void main(String... args) {
     System.out.printf("Hello %s.%n", args.length == 0 ? "Java" : args[0]);
-    System.out.printf("::set-output name=random-id::%d", new Random().nextInt(10));
+    System.out.println("::set-output name=random-id::" + new Random().nextGaussian());
     System.out.println("Goodbye");
   }
 }
