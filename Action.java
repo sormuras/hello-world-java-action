19df161e317eba2386fd5a105c6a9bb96c9e65cd
Christian Stein <sormuras@gmail.com>
2021-03-10 08:51:42 +0100

    Use `random-number` token consistently
    Addresses #1

--- Action.java
+++ Action.java
@@ -4,7 +4,7 @@ class Action {
   public static void main(String... args) {
     System.out.printf("Hello %s.%n", args.length == 0 ? "Java" : args[0]);
     var gaussian = new Random().nextGaussian();
-    System.out.println("::set-output name=random-id::" + gaussian);
+    System.out.println("::set-output name=random-number::" + gaussian);
     System.out.println("Goodbye and have fun with: " + gaussian);
   }
 }
